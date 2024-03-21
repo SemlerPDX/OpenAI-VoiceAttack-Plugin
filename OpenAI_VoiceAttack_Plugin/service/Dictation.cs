51fc4f7b7a5efe4a1b9e30d07996622d1b83b1d2
[=VG=] SemlerPDX <asemler09@gmail.com>
2024-03-21 12:21:40 -0700

    Updated Comments in Dictation.cs
    added copyright notice below class summary

--- OpenAI_VoiceAttack_Plugin/service/Dictation.cs
+++ OpenAI_VoiceAttack_Plugin/service/Dictation.cs
@@ -12,6 +12,24 @@ namespace OpenAI_VoiceAttack_Plugin
     /// <summary>
     /// A class providing methods for getting and storing User Input for the <see cref="ChatGPT"/> class.
     /// </summary>
+    /// <br>OpenAI API VoiceAttack Plugin</br>
+    /// <br>Copyright (C) 2023 Aaron Semler</br>
+    /// <br><see href="https://github.com/SemlerPDX">github.com/SemlerPDX</see></br>
+    /// <br><see href="https://veterans-gaming.com/semlerpdx-avcs">veterans-gaming.com/semlerpdx-avcs</see></br>
+    /// <br /><br />
+    /// This program is free software: you can redistribute it and/or modify<br />
+    /// it under the terms of the GNU General Public License as published by<br />
+    /// the Free Software Foundation, either version 3 of the License, or<br />
+    /// (at your option) any later version.<br />
+    /// <br />
+    /// This program is distributed in the hope that it will be useful,<br />
+    /// but WITHOUT ANY WARRANTY; without even the implied warranty of<br />
+    /// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the<br />
+    /// GNU General Public License for more details.<br />
+    /// <br />
+    /// You should have received a copy of the GNU General Public License<br />
+    /// along with this program.  If not, see <see href="https://www.gnu.org/licenses/">gnu.org/licenses</see>.
+    /// </para>
     public static class Dictation
     {
         private static readonly string DEFAULT_DICTATIONSTART_COMMAND = "((OpenAI DictationStart))";
